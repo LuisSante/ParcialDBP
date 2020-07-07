3117a240b75c4b933552d99717f8af49a1ded466
LuisSante <65087165+LuisSante@users.noreply.github.com>
2020-07-07 12:25:36 -0500

    CategoryRestServicies Json Jquery

---
+++ findadd.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+		
+ 		$.ajax({
+        	url: "http://localhost:8080/CategoryRestService/FindAll"
+    	}).then(function(data) {
+
+		$('#bt_add').click(function(){
+    		agregar;
+    	});
+    	var cont =0 ;
+    	function agregar(){
+    		cont++;
+    		var fila = '<tr class ="selected" id="fila'+cont
+    					+'"><td '+cont
+    					+'></td><td>texto</td><td>texto</td><td>texto</td></tr>';
+    		$('#tabla').append(fila); 
+    	}
+
+    	});
+
+});    	
